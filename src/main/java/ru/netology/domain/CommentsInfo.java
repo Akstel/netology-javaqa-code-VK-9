ec09bef4a15945ef9db9806582c97bd9c8001b54
Aleksey Stepin <diver911@yandex.ru>
2021-10-17 04:01:10 +0300

    file creation

---
+++ src/main/java/ru/netology/domain/CommentsInfo.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class CommentsInfo {
+    private int count;
+    private int canPost;
+    private boolean canClose;
+    private boolean can_Open;
+
+    // + get/set на все поля
+}
---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,17 @@
+package ru.netology.domain;
+
+public class Post {
+    private String id;
+    private String date;
+    private int ownerId;
+    private int fromId;
+    private int createdBy;
+    private String content;
+    private String imageUrl;
+    private String heading;
+    private int likes;
+    private String comments;
+    private int commentsNumber;
+
+    // + get/set на все поля
+}
